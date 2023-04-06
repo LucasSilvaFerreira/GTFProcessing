61fb4af60f59541ee1091c226649a4031e7b96e6
Lucas Ferreira da Silva <LucasSilvaFerreira@users.noreply.github.com>
2023-04-06 17:19:22 -0400

    fixing gtfparser version

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="GTFProcessing",
     version="0.0.1",
     py_modules=["GTFProcessing"],
-    install_requires=["pandas", "gtfparse", "tqdm"]
+    install_requires=["pandas", "gtfparse==1.3.0", "tqdm"]
 )
 
